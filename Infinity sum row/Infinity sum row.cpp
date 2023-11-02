d3d974ed109c56fb92a1386d0cd1a18a440e6495
Максим Ледовский <santech_montage@mail.ru>
2023-11-02 09:31:49 +0300

    -

--- Infinity sum row/Infinity sum row.cpp
+++ Infinity sum row/Infinity sum row.cpp
@@ -1,1 +1,43 @@
-﻿#
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	long double e, x; // e - эпсилон
+	int i = 3, fac = 1; // i - степень и факториал, fac - факториал числа
+
+	cout << "Расчитываем приближённое значение ln(1 - x) с точностью E" << endl;
+	cout << "Введите точность вычислений E: ";
+	cin >> e;
+	cout << "Введите x: ";
+	cin >> x;
+	while (x >= 1)
+	{
+		cout << "Попробуйте ещё раз!" << endl;
+		cout << "Введите x: ";
+		cin >> x;
+	}
+	
+
+	long double elem = -x, s = -x;
+
+	while (fabs(elem) >= e)
+	{
+		fac = fac * (i - 1);
+		elem = pow(x, i - 1) / fac;
+		cout << "elem" << i - 1 << " = " << elem << endl;
+		s -= elem;
+		cout << "s = " << s << endl;
+		i++;
+	}
+	cout << s;
+	return 0;
+}
